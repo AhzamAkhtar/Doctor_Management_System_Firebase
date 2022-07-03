0bfcc16d5868185a533b85d00a692e66ac3cf506
Ahzam Akhtar <ahzam8186@gmail.com>
2022-07-04 01:31:30 +0530

    fire-database

--- src/Component/Upload.js
+++ src/Component/Upload.js
@@ -1,48 +1,53 @@
-import React from "react";
-import { useState,useEffect} from "react";
-import {storage} from "./Firebase"
-import {ref,uploadBytes,listAll,getDownloadURL} from "firebase/storage"
-import {v4} from "uuid"
+import { useState, useEffect } from "react";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+  list,
+} from "firebase/storage";
+import { storage } from "./Firebase";
+import { v4 } from "uuid";
 import "./index.css"
-const Upload=()=>{
-    const [imageUpload,setImageUpload] = useState(null)
-    const [imageList,setImageList] = useState([])
-    const imageListRef = ref(storage,"images/")
-    const upload=()=>{
-        if(imageUpload==null) return;
-        const imageRef = ref(storage,`images/${imageUpload.name + v4()}`)
-        uploadBytes(imageRef,imageUpload).then((snapshot)=>{
-            getDownloadURL(snapshot.ref).then((url)=>{
-                setImageList((prev)=>[...prev,url])
-            })
-            //alert("Uploaded")
-        })
-    }
-    useEffect(()=>{
-        listAll(imageListRef).then((responce)=>{
-            //console.log(responce)
-            responce.items.forEach((item)=>{
-                getDownloadURL(item).then((url)=>{
-                    setImageList((prev)=>[...prev,url])
-                })
-            })
-        })
-    },[])
-    return(
-        <>
-        <div className="testing">
-        <div className="Appop">
-            <h1>Upload Images and Files To Our Cloud </h1>
-            <input type="file" onChange={(event)=>{setImageUpload(event.target.files[0])}}/>
-            <button onClick={upload}>Upload</button>
+function Upload() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
 
-            {imageList.map((url)=>{
-                return <img src={url}/>
-            })}
-        </div>
-        </div>
-        </>
+  const imagesListRef = ref(storage, "images/");
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
 
-    )
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
+  return (
+    <div className="Appop">
+      <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadFile}> Upload Image</button>
+      {imageUrls.map((url) => {
+        return <img src={url} />;
+      })}
+    </div>
+  );
 }
-export default Upload
+
+export default Upload;
